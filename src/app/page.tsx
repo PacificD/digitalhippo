60f3046a44b85dfbb21cf0276ae0c7b1555faf55
PacificD <1484636442@qq.com>
2023-11-26 11:00:30 +0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper>
+      <Button>ShadcnUI</Button>
+    </MaxWidthWrapper>
+  );
+}
